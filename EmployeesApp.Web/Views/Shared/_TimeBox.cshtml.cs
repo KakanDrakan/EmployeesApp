5dda12c58080e94dd8938e876c56bb7a9f402c00
Nidora <ludwigjohansson@gmail.com>
2025-05-19 13:56:03 +0200

    client side validation, current time

---
+++ EmployeesApp.Web/Views/Shared/_TimeBox.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmployeesApp.Web.Views.Shared
+{
+    public class _TimeBoxModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EmployeesApp.Web/wwwroot/scripts/MyScripts.js
@@ -0,0 +1,5 @@
+﻿//<div>
+//    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.7.1.min.js" asp-fallback-src="/scripts/lib/jquery/jquery.min.js"></script>
+//    <script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.19.2/jquery.validate.min.js"></script>
+//    <script src="https://ajax.aspnetcdn.com/ajax/mvc/5.2.3/jquery.validate.unobtrusive.min.js"></script>
+//</div>
